63d62d51540c4efb406fd9926b956b575a0ee113
FELLSAT <willysaty1393@hotmail.com>
2017-05-23 16:54:59 -0500

    index

--- index.php
+++ index.php
@@ -280,7 +280,29 @@
 
     <section id="contact" class="contact bg-primary">
         <!-- TWITTER-->
-
+        <?php
+ 
+        /* Iniciando la sesión*/
+        session_start();
+         
+        /* Cambiar según la ubicación de tu directorio*/
+        require_once __DIR__ . '/facebook/src/Facebook/autoload.php';
+         
+        $fb = new Facebook\Facebook([
+          'app_id' => '428124097567667',
+          'app_secret' => '6447f81ed44dc3a258ccd223576abde5',
+          'default_graph_version' => 'v2.9',
+        ]);
+          
+        $helper = $fb->getRedirectLoginHelper();
+          
+        $permissions = ['email']; // Permisos opcionales
+        $loginUrl = $helper->getLoginUrl('http://www.legendofanime.com ', $permissions);
+          
+        /* Link a la página de login*/
+        echo '<a href="' . htmlspecialchars($loginUrl) . '">Login con Facebook!</a>';
+         
+?>
         <!--<a class="twitter-timeline" href="https://twitter.com/UCooperativaCol">Tweets by UCooperativaCol</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>-->
         <!-- TWITTER-->
 
